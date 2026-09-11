d747ac148b6ae25f7c871211e360dbb8c49cb85d
kragom <119004638+kragom@users.noreply.github.com>
2026-09-11 14:59:56 +0200

    deploy: app/static/sw.js

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'balance-v8';
+const CACHE = 'balance-v9';
 const ASSETS = ['/static/styles.css', '/static/app.js', '/static/icon.svg', '/static/manifest.json'];
 
 self.addEventListener('install', (e) => {
