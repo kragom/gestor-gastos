724078ce5190b6497d1319e8b608ae5908c07a3d
kragom <119004638+kragom@users.noreply.github.com>
2026-09-11 15:28:21 +0200

    layout: dock flotante + inicio bento

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'balance-v11';
+const CACHE = 'balance-v12';
 const ASSETS = ['/static/styles.css', '/static/app.js', '/static/icon.svg', '/static/manifest.json'];
 
 self.addEventListener('install', (e) => {
