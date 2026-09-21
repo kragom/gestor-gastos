93978703f85863a5ce82ef8cc55f58ea3f279f3b
kragom <119004638+kragom@users.noreply.github.com>
2026-09-21 11:13:52 +0200

    Filtros de movimientos como desplegable - app/static/sw.js

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'balance-v18';
+const CACHE = 'balance-v19';
 const ASSETS = ['/static/styles.css', '/static/app.js', '/static/icon.svg', '/static/manifest.json'];
 
 self.addEventListener('install', (e) => {
